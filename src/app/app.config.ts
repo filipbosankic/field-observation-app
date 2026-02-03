a2786349a85fef58ae77e97cfe4e11605b34d11f
filip bosankic <48357512+filipbosankic@users.noreply.github.com>
2026-02-03 16:23:34 +0100

    feat(i18n): set de-CH locale for consistent date and time formatting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { registerLocaleData } from '@angular/common';
+import localeDeCh from '@angular/common/locales/de-CH';
+import { routes } from './app.routes';
+
+registerLocaleData(localeDeCh);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })
+    provideRouter(routes),
+    
+    { provide: LOCALE_ID, useValue: 'de-CH' },
+    
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };
